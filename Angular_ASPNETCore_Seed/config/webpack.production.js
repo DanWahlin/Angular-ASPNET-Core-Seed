1a730e13d0ae19dd83eb90a5829fd223f8e9fcb0
Dan Wahlin <danwahlin@Dans-iMac.local>
2017-08-15 13:51:33 -0700

    Update webpack config and added settings for vs code

--- Angular_ASPNETCore_Seed/config/webpack.production.js
+++ Angular_ASPNETCore_Seed/config/webpack.production.js
@@ -1,53 +1,53 @@
 const webpack = require('webpack'),
-webpackMerge = require('webpack-merge'),
-ExtractTextPlugin = require('extract-text-webpack-plugin'),
-commonConfig = require('./webpack.common.js'),
-ngToolsWebpack = require('@ngtools/webpack'),
-path = require('path'),
-rootDir = path.resolve(__dirname, '..');
+  webpackMerge = require('webpack-merge'),
+  ExtractTextPlugin = require('extract-text-webpack-plugin'),
+  commonConfig = require('./webpack.common.js'),
+  ngToolsWebpack = require('@ngtools/webpack'),
+  path = require('path'),
+  rootDir = path.resolve(__dirname, '..');
 
 var aotPlugin = new ngToolsWebpack.AotPlugin({
-tsConfigPath: "./tsconfig.aot.json",
-entryModule: path.resolve(__dirname, "./wwwroot/app/app.module#AppModule"),
+  tsConfigPath: "./tsconfig.aot.json",
+  entryModule: path.resolve(rootDir, "wwwroot/app/app.module#AppModule"),
 });
 
 const env = process.env.NODE_ENV;
 
 module.exports = webpackMerge(commonConfig, {
-entry: {
-app: './wwwroot/app/main.aot.ts'
-},
-output: {
-path: path.resolve(rootDir, 'wwwroot/dist'),
-publicPath: '/dist/',
-filename: '[name].js',
-chunkFilename: '[id].chunk.js'
-},
-module: {
-loaders: [{
-test: /\.ts$/,
-loader: '@ngtools/webpack'
-}]
-}, 
-plugins: [
-//Angular AOT pluging
-// new ngToolsWebpack.AotPlugin({
-//     mainPath: "src/app/main.ts",
-//     tsConfigPath: './tsconfig.aot.json'
-// }),
-aotPlugin,
-new webpack.LoaderOptionsPlugin({
-  minimize: true,
-  debug: false
-}),
-new webpack.optimize.UglifyJsPlugin({
-  compress: {
-      warnings: false
+  entry: {
+    app: './wwwroot/app/main.aot.ts'
   },
   output: {
-      comments: false
+    path: path.resolve(rootDir, 'wwwroot/dist'),
+    publicPath: '/dist/',
+    filename: '[name].js',
+    chunkFilename: '[id].chunk.js'
   },
-  sourceMap: false
-})
-]
+  module: {
+    loaders: [{
+      test: /\.ts$/,
+      loader: '@ngtools/webpack'
+    }]
+  },
+  plugins: [
+    //Angular AOT pluging
+    // new ngToolsWebpack.AotPlugin({
+    //     mainPath: "src/app/main.ts",
+    //     tsConfigPath: './tsconfig.aot.json'
+    // }),
+    aotPlugin,
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    })
+  ]
 });
